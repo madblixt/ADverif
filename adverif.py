14b50eadd9c3ae8e64020abc1f90b4560fef5973
MadBlixt <94877777+madblixt@users.noreply.github.com>
2022-12-29 16:58:53 +0100

    Update adverif.py

--- adverif.py
+++ adverif.py
@@ -25,19 +25,37 @@ for dn, entry in result:
         print("Erreur de recherche LDAP : " + str(e))
         continue
 
+    # Check for login attempts from unfamiliar IP addresses
+    if 'dNSHostName' in entry:
+        ip_addresses = entry['dNSHostName']
+        if len(ip_addresses) > 1:
+            # Send an alert to the administrator if the user has logged in from multiple IP addresses
+            try:
+                from_address = 'noreply@example.com'
+                to_address = 'admin@example.com'
+                subject = 'Suspicious login activity'
+                body = 'User ' + entry['cn'][0] + ' has logged in from multiple IP addresses: ' + ', '.join(ip_addresses)
+                message = 'Subject: {}\n\n{}'.format(subject, body)
+                smtp_conn = smtplib.SMTP('smtp.example.com')
+                smtp_conn.sendmail(from_address, to_address, message)
+            except smtplib.SMTPException as e:
+                print("Erreur d'envoi d'e-mail : " + str(e))
 
-# Send an alert to the administrator if the user has had more than 5 failed login attempts
-try:
-    from_address = 'noreply@example.com'
-    to_address = 'admin@example.com'
-    subject = 'Suspicious login activity'
-    body = 'User ' + entry['cn'][0] + ' has had ' + str(bad_password_count) + ' failed login attempts.'
-    message = 'Subject: {}\n\n{}'.format(subject, body)
-    smtp_conn = smtplib.SMTP('smtp.example.com')
-    smtp_conn.sendmail(from_address, to_address, message)
-except smtplib.SMTPException as e:
-    print("Erreur d'envoi d'e-mail : " + str(e))
+    # Check for a sudden increase in failed login attempts
+    if 'badPwdCount' in login_history:
+        bad_password_count = int(login_history['badPwdCount'][0])
+        if bad_password_count > 5:
+            # Send an alert to the administrator if the user has had more than 5 failed login attempts
+            try:
+                from_address = 'noreply@example.com'
+                to_address = 'admin@example.com'
+                subject = 'Suspicious login activity'
+                body = 'User ' + entry['cn'][0] + ' has had ' + str(bad_password_count) + ' failed login attempts.'
+                message = 'Subject: {}\n\n{}'.format(subject, body)
+                smtp_conn = smtplib.SMTP('smtp.example.com')
+                smtp_conn.sendmail(from_address, to_address, message)
+            except smtplib.SMTPException as e:
+                print("Erreur d'envoi d'e-mail : " + str(e))
 
 # Disconnect from the server
 ldap_conn.unbind_s()
-
